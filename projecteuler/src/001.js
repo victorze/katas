a44718a0f70764049dc4d8a86fa63b6c9a91f6ff
Victor <victorze@protonmail.com>
2021-07-03 23:45:38 -0500

    Optimize solution 1

--- projecteuler/src/001.js
+++ projecteuler/src/001.js
@@ -1,15 +1,13 @@
 function sumOfMultiplesOfThreeOrFive(top) {
-  let sum = 0;
+  return sumDivisibleBy(3, top - 1) + sumDivisibleBy(5, top - 1);
+}
 
-  for (let i = 1; i < top; i++) {
-    if (i % 3 == 0 || i % 5 == 0) {
-      sum += i;
-    }
-  }
-
-  return sum;
+function sumDivisibleBy(n, target) {
+  p = Math.floor(target / n);
+  return n * (p * (p + 1)) / 2;
 }
 
 console.log("Answer:", sumOfMultiplesOfThreeOrFive(1000));
+console.log(sumOfMultiplesOfThreeOrFive(1000000000));
 
 module.exports = sumOfMultiplesOfThreeOrFive;
